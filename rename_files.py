3bca980339b35f30a5cca3664d914594b42f13ba
bogadodiegoh <diegobogado@live.com.ar>
2017-03-13 22:48:42 -0300

    Updated text in print command

--- rename_files.py
+++ rename_files.py
@@ -3,7 +3,7 @@ def rename_files():
     file_list = os.listdir(r"carpeta_de_imagenes")
     print(file_list)
     saved_path = os.getcwd()
-    print("Current Workin Directory is " + saved_path)
+    print("Current Working Directory is " + saved_path)
     os.chdir(r"carpeta_de_imagenes")
     for file_name in file_list:
         os.rename(file_name,file_name.translate(str.maketrans('','','0123456789')))
